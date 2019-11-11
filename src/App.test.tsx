a49cd53792e1be72e2a1008d71ccafeebf8b9577
Ken Sodemann <ken@ionic.io>
2019-11-11 14:56:42 -0600

    training(unit-tests): add basic unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const { container } = render(<App />);
+  expect(container.innerHTML).toBeTruthy();
+});
+
+it.each([
+  [0, "Tab One"],
+  [1, "Tab Two"],
+  [2, "Tab Three"]
+])("contains the proper text for tab %i", (tab, text) => {
+  const { container } = render(<App />);
+  expect(
+    container.querySelectorAll("ion-tab-button")[tab as number].textContent
+  ).toEqual(text);
+});
+
+it('renders consistently', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
 });
